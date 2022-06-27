1cea999b0a6bb0c17e19db99f839607d1496c3fe
ryann127 <ryann.goldberg127@gmail.com>
2022-06-27 14:04:53 -0600

    establishing user model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,39 @@
 const User = require('./User');
 const Thought = require('./Thought');
 
+// Schema to create Student model
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        //validation
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Thought',
+        },
+      ],
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      ],
+      },
+    {
+      toJSON: {
+        getters: true,
+      },
+    }
+  );
+
 module.exports = { User, Thought };
